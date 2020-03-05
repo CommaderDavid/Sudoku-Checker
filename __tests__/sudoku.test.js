d5531ed67c45c294ca7b967a580119fcdfad73b4
David Boedigheimer <stealth259@gmail.com>
2020-03-05 14:59:25 -0800

    Add test for seeing the sudoku board.

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,1 +1,15 @@
 // use "debugger" to look for bugs and broken code.
+import { sudoku } from './../src/sudoku.js'
+
+describe('sudoku board', () => {
+
+  test('should find sudoku board', () => {
+    var sud = sudoku;
+    expect(sud).toContain(sudoku[0]);
+  });
+
+  test('should check for number in rows to be 1-9, no repeats', () => {
+    var sud = sudoku;
+    
+  });
+});
