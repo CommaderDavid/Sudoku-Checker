721b0e5b45fb46714bd950fff261f666e7662621
David Boedigheimer <stealth259@gmail.com>
2020-03-09 15:56:03 -0700

    Add strange loop that causes strange results.

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -1,16 +1,34 @@
 // use "debugger" to look for bugs and broken code.
 import { sudoku } from './../src/sudoku.js'
+import { LookRow } from './../src/sudoku.js'
 
 describe('sudoku board', () => {
+  const sud = sudoku.slice();
+  console.log(sudoku);
+  console.log(sud);
+  const row = LookRow(sud);
+  console.log(row, "ROW");
+  console.log(sud);
+  console.log(sudoku);
 
   test('should find sudoku board', () => {
-    var sud = sudoku;
     expect(sud).toContain(sudoku[0]);
   });
-
-  test('should check for number in rows to be 1-9, no repeats', () => {
-    
-    expect(sudoku[0]).toContainEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-
-  });
 });
+
+// describe('LookRow', () => {
+//   var sud2 = sudoku.slice();
+//   console.log(sud2);
+//   var row2 = LookRow(sud2);
+//   console.log(row2);
+//
+//   test('should check for number in rows to be 1-9, no repeats', () => {
+//     expect(row2).toContainEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+//
+//   });
+//
+//   // test('should see myDict', () => {
+//   //   var sud = sudoku;
+//   //   var row = LookRow(sud);
+//   // })
+// });
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,11 +1,51 @@
-var col1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var col2 = [2, 3, 4, 5, 6, 7, 8, 9, 1];
-var col3 = [3, 4, 5, 6, 7, 8, 9, 1, 2];
-var col4 = [4, 5, 6, 7, 8, 9, 1, 2, 3];
-var col5 = [5, 6, 7, 8, 9, 1, 2, 3, 4];
-var col6 = [6, 7, 8, 9, 1, 2, 3, 4, 5];
-var col7 = [7, 8, 9, 1, 2, 3, 4, 5, 6];
-var col8 = [8, 9, 1, 2, 3, 4, 5, 6, 7];
-var col9 = [9, 1, 2, 3, 4, 5, 6, 7, 8];
-export var sudoku = [col1, col2, col3, col4, col5, col6, col7, col8, col9];
+const col1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const col2 = [2, 3, 4, 5, 6, 7, 8, 9, 1];
+const col3 = [3, 4, 5, 6, 7, 8, 9, 1, 2];
+const col4 = [4, 5, 6, 7, 8, 9, 1, 2, 3];
+const col5 = [5, 6, 7, 8, 9, 1, 2, 3, 4];
+const col6 = [6, 7, 8, 9, 1, 2, 3, 4, 5];
+const col7 = [7, 8, 9, 1, 2, 3, 4, 5, 6];
+const col8 = [8, 9, 1, 2, 3, 4, 5, 6, 7];
+const col9 = [9, 1, 2, 3, 4, 5, 6, 7, 8];
+export const sudoku = [col1, col2, col3, col4, col5, col6, col7, col8, col9];
 // total = 45
+
+export function LookRow(col) {
+  const testArray = col.slice();
+  console.log(col, "hi");
+
+  // console.log(testArray + "before");
+  // testArray.forEach(array => {
+  //   array.sort(function(a, b){return a-b})
+  // });
+  // console.log(testArray + "after");
+
+  const findRow = testArray.map(array => {
+    for (let i = 0; i < array.length; i++) {
+      console.log(array, "problem");
+      if (array[i] === 1) {
+        array[i] = 0
+      } else if (array[i] === 2) {
+        array[i] = 0
+      } else if (array[i] === 3) {
+        array[i] = 0
+      } else if (array[i] === 4) {
+        array[i] = 0
+      } else if (array[i] === 5) {
+        array[i] = 0
+      } else if (array[i] === 6) {
+        array[i] = 0
+      } else if (array[i] === 7) {
+        array[i] = 0
+      } else if (array[i] === 8) {
+        array[i] = 0
+      } else if (array[i] === 9) {
+        array[i] = 0
+      }
+    }
+    return array;
+  });
+  console.log(col, "two");
+  console.log(findRow, "three");
+  return findRow;
+}
