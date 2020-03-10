e7e6d959047915eb483cd0020559252cb2fc47f0
David Boedigheimer <stealth259@gmail.com>
2020-03-10 10:53:06 -0700

    Add fix to cloning problem.

--- __tests__/sudoku.test.js
+++ __tests__/sudoku.test.js
@@ -3,32 +3,25 @@ import { sudoku } from './../src/sudoku.js'
 import { LookRow } from './../src/sudoku.js'
 
 describe('sudoku board', () => {
-  const sud = sudoku.slice();
-  console.log(sudoku);
-  console.log(sud);
+  const sud = sudoku;
   const row = LookRow(sud);
-  console.log(row, "ROW");
-  console.log(sud);
-  console.log(sudoku);
 
   test('should find sudoku board', () => {
     expect(sud).toContain(sudoku[0]);
   });
 });
 
-// describe('LookRow', () => {
-//   var sud2 = sudoku.slice();
-//   console.log(sud2);
-//   var row2 = LookRow(sud2);
-//   console.log(row2);
-//
-//   test('should check for number in rows to be 1-9, no repeats', () => {
-//     expect(row2).toContainEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-//
-//   });
-//
-//   // test('should see myDict', () => {
-//   //   var sud = sudoku;
-//   //   var row = LookRow(sud);
-//   // })
-// });
+describe('LookRow', () => {
+  var sud2 = sudoku;
+  var row2 = LookRow(sud2);
+
+  test('should check for number in rows to be 1-9, no repeats', () => {
+    expect(row2[0]).toContainEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+  });
+
+  // test('should see myDict', () => {
+  //   var sud = sudoku;
+  //   var row = LookRow(sud);
+  // })
+});
--- src/sudoku.js
+++ src/sudoku.js
@@ -1,4 +1,5 @@
-const col1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const col1 = [1, 2, 3, 4, 5, 5, 7, 8, 9];
+// const col1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 const col2 = [2, 3, 4, 5, 6, 7, 8, 9, 1];
 const col3 = [3, 4, 5, 6, 7, 8, 9, 1, 2];
 const col4 = [4, 5, 6, 7, 8, 9, 1, 2, 3];
@@ -9,10 +10,10 @@ const col8 = [8, 9, 1, 2, 3, 4, 5, 6, 7];
 const col9 = [9, 1, 2, 3, 4, 5, 6, 7, 8];
 export const sudoku = [col1, col2, col3, col4, col5, col6, col7, col8, col9];
 // total = 45
+const checker = [];
 
 export function LookRow(col) {
-  const testArray = col.slice();
-  console.log(col, "hi");
+  const testArray = JSON.parse(JSON.stringify(col));
 
   // console.log(testArray + "before");
   // testArray.forEach(array => {
@@ -22,7 +23,6 @@ export function LookRow(col) {
 
   const findRow = testArray.map(array => {
     for (let i = 0; i < array.length; i++) {
-      console.log(array, "problem");
       if (array[i] === 1) {
         array[i] = 0
       } else if (array[i] === 2) {
@@ -45,7 +45,5 @@ export function LookRow(col) {
     }
     return array;
   });
-  console.log(col, "two");
-  console.log(findRow, "three");
   return findRow;
 }
